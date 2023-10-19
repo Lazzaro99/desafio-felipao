d5fe652fa2ae15a8a290f1e7f1c8229a8dadda96
Lazzaro99 <lazaro1928c@gmail.com>
2023-10-19 19:43:26 -0300

    desafio DIO

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-asdasd
+let Heroi = "Arqueiro Guardião";
+let Nivel = ""
+let random = Math.floor(Math.random() * 10.001) + 1.000;
+let XP = 0
+while ( XP < random) {
+    XP+= 1
+}
+
+if (XP <= 1.000) {
+    Nivel = "Ferro"
+} else if (XP >= 1.001 && XP <= 2.000) {
+    Nivel = "Bronze"
+} else if (XP >= 2.001 && XP <= 5.000) {
+    Nivel = "Prata"
+} else if (XP >= 5.001 && 7.000) {
+    Nivel = "Ouro"
+} else if (XP >= 7.001 && 8.000) {
+    Nivel = "Platina"
+} else if (XP >= 8.001 && 9.000) {
+    Nivel = "Ascendente"
+} else if (XP >= 9.001 && 10.000) {
+    Nivel = "Imortal"
+} else if (XP >= 10.001) {
+    Nivel = "Radiante"
+}
+
+console.log("O Heroi de nome " +Heroi+ " está no nível de " +Nivel);
